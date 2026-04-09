e14fdd6f55b66b8df448ba8edbbf140c3ab1ed68
Jack Guptill <123787387+jguptil1@users.noreply.github.com>
2026-04-09 15:33:44 -0400

    api data pipeline update 4/9

---
+++ src/ingestion/race_meetings.py
@@ -0,0 +1,243 @@
+
+from urllib.request import urlopen
+from pathlib import Path
+import duckdb
+import requests
+import time
+import pandas as pd
+
+
+
+##################################Ingestion Layer########################################
+
+
+
+#helper functions
+def get_raw_meetings(year, max_retries = 10, sleep_seconds=2):
+
+    '''
+    this function creates a pandas dataframe of meeting info for a given year
+    returns: pandas dataframe
+    '''
+
+    url = "https://api.openf1.org/v1/meetings"
+    params = {
+        "year": year
+    }
+
+    #throttling call
+    for attempt in range(max_retries):
+        response = requests.get(url, params=params, timeout=30)
+
+
+        #not a valid session_key
+        if response.status_code == 404:
+            raise requests.exceptions.HTTPError(
+                f"404 not found for year:{year}"
+            )
+        
+        #rate limit hit
+        if response.status_code == 429:
+            wait = sleep_seconds * (attempt +1)
+            print(f"Rate Limited on year={year}. Sleeping {wait} seconds")
+            time.sleep(wait)
+            continue
+            
+        response.raise_for_status()
+        return pd.DataFrame(response.json())
+    
+    raise requests.exceptions.HTTPError(
+        f"429 persisted after {max_retries} retries for year={year}",
+        response=response
+    )
+
+
+def read_raw_meetings_table():
+
+    con = duckdb.connect("data/database/f1_fantasy.duckdb")
+    result = con.execute("SELECT * FROM raw_meetings_table").df()
+
+    return result
+
+
+def get_unique_years_in_raw_meetings_table():
+    con = duckdb.connect("data/database/f1_fantasy.duckdb")
+    result = con.execute("SELECT DISTINCT year FROM raw_meetings_table").fetchall()
+    years = [row[0] for row in result]
+    return years
+
+
+#API CALL
+def get_unique_meeting_ids_raw_table():
+    con = duckdb.connect("data/database/f1_fantasy.duckdb")
+    result = con.execute("SELECT DISTINCT meeting_key FROM raw_meetings_table")
+    return result
+
+#LOAD
+def write_raw_meetings_table(df):
+
+    con = duckdb.connect("data/database/f1_fantasy.duckdb")
+
+    con.register("meetings_raw_df_temp", df)
+
+    result = con.execute("""
+    CREATE OR REPLACE TABLE raw_meetings_table AS
+    SELECT *
+    FROM meetings_df_temp
+    """)
+
+    con.close()
+
+    return result
+
+#RAW CONTROLLER
+def build_raw_race_meetings_controller(years = [2023, 2024, 2025, 2026]):
+
+    '''
+    this function controls getting the various meeting ids and writing to the database
+    - this is a one time use to build the meetings and to push it to the database
+    - for simple update/append of the meetings ids, another function should be used instead to increase efficiency
+    '''
+
+    compiled_race_sessions_df = pd.DataFrame()
+
+    for year in years:
+        year_df = get_raw_meetings(year)
+        compiled_race_sessions_df = pd.concat(
+            [compiled_race_sessions_df, year_df],
+            ignore_index=True
+        )
+
+    
+    write_raw_meetings_table(compiled_race_sessions_df)
+
+def update_raw_race_meetings_controller(year = 2026): 
+    compiled_race_sessions_df = pd.DataFrame()
+
+    for year in years:
+        year_df = get_raw_meetings(year)
+        compiled_race_sessions_df = pd.concat(
+            [compiled_race_sessions_df, year_df],
+            ignore_index=True
+        )
+
+    
+    write_raw_meetings_table(compiled_race_sessions_df)
+
+##########################Staging Layer#######################
+
+#rename columns
+#standardize data types
+#filter unwanted rows/columns
+    #testing and canceled events
+#deduplicate
+#reshape wide to long
+#standardize names/codes
+#add simple derived fields needed for joins
+#align grains
+#prepare keys for warehouse layer
+
+
+#helper functions
+
+#pull in the raw file
+def pull_raw_meetings():
+
+    con = duckdb.connect("data/database/f1_fantasy.duckdb")
+    result = con.execute("SELECT * FROM raw_meetings_table").df()
+
+    return result
+
+#cleaning the raw file
+def clean_raw_meetings(df):
+
+    """
+    1. dedup
+    2. filter rows and cols
+    3. data type conversions
+    
+    """
+
+    #dedup
+    df = df.drop_duplicates(subset=["meeting_key"])
+
+    #filter rows
+    
+    ##taking out testing meetings or meetings that got canceled
+    df = df[
+        ~df["meeting_official_name"].str.contains(
+            "called off|test",
+            case=False,
+            na=False
+        )
+    ]
+
+    #filter cols
+    cols_to_keep = ["meeting_key", "meeting_name", "country_name", "circuit_short_name", "circuit_type", "date_start", "date_end", "year"]
+    df = df[[col for col in cols_to_keep if col in df.columns]]
+
+
+    #data type conversions
+    df["date_start"] = pd.to_datetime(df["date_start"], utc=True, errors="coerce")
+    df["date_end"] = pd.to_datetime(df["date_start"], utc=True, errors="coerce")  
+
+    return df
+
+
+#writing the staged table to the database
+def build_stage_meetings_controller():
+
+    raw_df = pull_raw_meetings()
+
+    stage_df = clean_raw_meetings(raw_df)
+
+    con = duckdb.connect("data/database/f1_fantasy.duckdb")
+    con.register("meetings_staged_df_temp", stage_df)
+
+    result = con.execute("""
+        CREATE OR REPLACE TABLE staged_race_meetings_table AS
+        SELECT *
+        FROM meetings_staged_df_temp
+        """)
+
+    con.close()
+
+    return result
+
+
+
+
+############################Pipeline Controller###############
+
+def meetings_pipeline(update:bool):
+
+    '''
+    update toggle helps with decreasing API Call volume
+    '''
+
+    if not update:
+        #building the raw table
+        raw_df = build_raw_race_meetings_controller(2023,2024,2025,2026)
+        write_raw_meetings_table(raw_df)
+
+        #stage
+        build_stage_meetings_controller()
+
+    else:
+        #building the raw table
+        raw_df = update_raw_race_meetings_controller(year = 2026)
+        write_raw_meetings_table(raw_df) #will contain dups
+
+        #stage
+        build_raw_race_meetings_controller()
+
+
+
+
+if __name__ == "__main__":
+    #build_raw_race_meetings_controller()
+    build_stage_meetings_controller()
+    # show_driver_table()
+    #race_sessions_database_controller()
+    #print(read_meetings_table())
+    
--- src/ingestion/race_sessions.py
+++ src/ingestion/race_sessions.py
@@ -1,4 +1,3 @@
-
 from urllib.request import urlopen
 from pathlib import Path
 import duckdb
@@ -7,121 +6,28 @@ import time
 import pandas as pd
 
 
-#helper functions
-def get_meetings_info(year, max_retries = 10, sleep_seconds=2):
 
-    '''
-    this function creates a pandas dataframe of meeting info for a given year
-    returns: pandas dataframe
-    '''
-
-    url = "https://api.openf1.org/v1/meetings"
-    params = {
-        "year": year
-    }
-
-    #throttling call
-    for attempt in range(max_retries):
-        response = requests.get(url, params=params, timeout=30)
+##################################Ingestion Layer########################################
 
 
-        #not a valid session_key
-        if response.status_code == 404:
-            raise requests.exceptions.HTTPError(
-                f"404 not found for year:{year}"
-            )
-        
-        #rate limit hit
-        if response.status_code == 429:
-            wait = sleep_seconds * (attempt +1)
-            print(f"Rate Limited on year={year}. Sleeping {wait} seconds")
-            time.sleep(wait)
-            continue
-            
-        response.raise_for_status()
-        return pd.DataFrame(response.json())
-    
-    raise requests.exceptions.HTTPError(
-        f"429 persisted after {max_retries} retries for year={year}",
-        response=response
-    )
+#helper function to get all of the unique meeting ids
 
-
-def write_meetings_table(df):
-
+def get_meeting_ids():
     con = duckdb.connect("data/database/f1_fantasy.duckdb")
-
-    con.register("meetings_df_temp", df)
-
-    result = con.execute("""
-    CREATE OR REPLACE TABLE meetings AS
-    SELECT *
-    FROM meetings_df_temp
-    """)
-
-    con.close()
+    result = con.execute("SELECT DISTINCT meeting_key FROM staged_race_meetings_table").df()["meeting_key"].tolist()
 
     return result
 
 
+def get_raw_race_sessions(meeting_ids):
 
-def race_sessions_database_controller(years = [2023, 2024, 2025, 2026]):
-
-    '''
-    this function controls getting the various meeting ids and writing to the database
-    - this is a one time use to build the meetings and to push it to the database
-    - for simple update/append of the meetings ids, another function should be used instead to increase efficiency
-    '''
-
-    compiled_race_sessions_df = pd.DataFrame()
-
-    for year in years:
-        year_df = get_meetings_info(year)
-        compiled_race_sessions_df = pd.concat(
-            [compiled_race_sessions_df, year_df],
-            ignore_index=True
-        )
-
-    
-    write_meetings_table(compiled_race_sessions_df)
-
-
-
-def read_meetings_table():
-
-    con = duckdb.connect("data/database/f1_fantasy.duckdb")
-    result = con.execute("SELECT * FROM meetings").df()
-
-    return result
-
-
-#### Helpful get functions for other modules
-
-
-def get_unique_years_in_meetings_table():
-    con = duckdb.connect("data/database/f1_fantasy.duckdb")
-    result = con.execute("SELECT DISTINCT year FROM meetings").fetchall()
-    years = [row[0] for row in result]
-    return years
-
-
-def get_unique_meeting_ids():
-    con = duckdb.connect("data/database/f1_fantasy.duckdb")
-    result = con.execute("SELECT DISTINCT meeting_key FROM meetings")
-    return result
-
+    #throttling happens here 
+    pass
 
 
 if __name__ == "__main__":
-    # load_dim_driver()
+    #build_raw_race_meetings_controller()
+    get_meeting_ids()
     # show_driver_table()
     #race_sessions_database_controller()
     #print(read_meetings_table())
-
-
-
-
-    years = get_unique_years_in_meetings_table()
-    print(years)
-
-    
